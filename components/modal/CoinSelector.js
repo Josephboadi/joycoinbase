9373efc4774000db84bc7fa4e3c6c8d612fc4017
Josephboadi <boadijoseph151@gmail.com>
2022-02-24 11:16:12 +0100

    new

---
+++ components/modal/CoinSelector.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import CoinItem from "./CoinItem";
+
+const CoinSelector = ({
+  setAction,
+  selectedToken,
+  setSelectedToken,
+  sanityTokens,
+  thirdWebTokens,
+  walletAddress,
+}) => {
+  return (
+    <Wrapper>
+      <Title>Select Asset</Title>
+      <CoinList>
+        {sanityTokens.map((token, index) => (
+          <CoinItem
+            token={token}
+            key={index}
+            sender={walletAddress}
+            selectedToken={selectedToken}
+            setSelectedToken={setSelectedToken}
+            thirdWebTokens={thirdWebTokens}
+            sanityTokens={sanityTokens}
+            setAction={setAction}
+          />
+        ))}
+      </CoinList>
+    </Wrapper>
+  );
+};
+
+export default CoinSelector;
+
+const Wrapper = styled.div``;
+const Title = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+`;
+const CoinList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
